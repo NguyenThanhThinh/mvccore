1c3c46f8f404558cd51bbff0dd603524c7f31035
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2018-05-18 00:31:02 +0700

    add logic controller

--- mvccore/PartyInvites/Controllers/HomeController.cs
+++ mvccore/PartyInvites/Controllers/HomeController.cs
@@ -1,16 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PartyInvites.Controllers
 {
+    using PartyInvites.Models;
     public class HomeController : Controller
     {
+        private DataContext db;
+
+        protected HomeController(DataContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Respond() => View();
+
+        [HttpPost]
+        public IActionResult Respond(RuestResponse ruestResponse)
+        {
+            db.Responses.Add(ruestResponse);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Thanks),
+           new
+           {
+               Name = ruestResponse.Name,
+               WillAttend = ruestResponse.WillAttend
+           });
+        }
+
+        public IActionResult Thanks() => View();
+
+        public IActionResult ListResponses()
+        {
+            return View(db.Responses.OrderByDescending(r => r.WillAttend));
         }
     }
 }
