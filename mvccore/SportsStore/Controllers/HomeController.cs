c3c815b7ed12bc0ada439f32ff8dccadf395f841
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2018-05-19 23:34:48 +0700

    update all

--- mvccore/SportsStore/Controllers/HomeController.cs
+++ mvccore/SportsStore/Controllers/HomeController.cs
@@ -14,15 +14,40 @@ namespace SportsStore.Controllers
 
         public IActionResult Index()
         {
-            
             var results = _repository.Products;
-          
+
             return View(results);
         }
 
+        [HttpPost]
         public IActionResult AddProduct(Product product)
         {
             _repository.AddProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UpdateProduct(long key)
+        {
+            return View(_repository.GetProduct(key));
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _repository.UpdateProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UpdateAll()
+        {
+            ViewBag.UpdateAll = true;
+            return View(nameof(Index), _repository.Products);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAll(Product[] products)
+        {
+            _repository.UpdateAll(products);
             return RedirectToAction(nameof(Index));
         }
     }
--- mvccore/SportsStore/Models/ProductRepository.cs
+++ mvccore/SportsStore/Models/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using SportsStore.Data;
 
@@ -18,6 +19,29 @@ namespace SportsStore.Models
         public void AddProduct(Product product)
         {
             DbContext.Add(product);
+            DbContext.SaveChanges();
+        }
+
+        public Product GetProduct(long key)
+        {
+            if (key <= 0) throw new ArgumentOutOfRangeException(nameof(key));
+            return DbContext.Products.Find(key);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            //var p = GetProduct(product.Id);
+            //p.Name = product.Name;
+            //p.Category = product.Category;
+            //p.PurchasePrice = product.PurchasePrice;
+            //p.RetailPrice = product.RetailPrice;
+            DbContext.Products.Update(product);
+            DbContext.SaveChanges();
+        }
+
+        public void UpdateAll(Product[] products)
+        {
+            DbContext.Products.UpdateRange(products);
             DbContext.SaveChanges();
         }
     }
