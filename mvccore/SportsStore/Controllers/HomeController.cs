2ebac3c05ba0b4bcd8fa5df05da242518d449fca
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2018-05-19 21:53:26 +0700

    add model and controller

---
+++ mvccore/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var results = _repository.Products;
+            return View(results);
+        }
+
+        public IActionResult AddProduct(Product product)
+        {
+            _repository.AddProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
