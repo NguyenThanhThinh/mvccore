76611c165de8cf539aeeb22bec82a0342a1e7cd9
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2018-05-21 22:08:15 +0700

    add

--- mvccore/SportsStore/Migrations/20180521150245_orders.Designer.cs
+++ mvccore/SportsStore/Migrations/20180521150245_orders.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(SportStoreDbContext))]
-    partial class SportStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180521150245_orders")]
+    partial class orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
